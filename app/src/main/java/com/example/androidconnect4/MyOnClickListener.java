00a468b21feae7d624941a3e18ed2df142b62163
Jordi Blanco Lopez <jordi14and@gmail.com>
2019-05-07 12:35:19 +0200

    Implementation for the GRID VIEW of the game

---
+++ app/src/main/java/com/example/androidconnect4/MyOnClickListener.java
@@ -0,0 +1,15 @@
+package com.example.androidconnect4;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MyOnClickListener implements View.OnClickListener {
+
+    public MyOnClickListener(int position, Activity mContext) {
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/example/androidconnect4/Variables.java
+++ app/src/main/java/com/example/androidconnect4/Variables.java
@@ -6,4 +6,5 @@ public class Variables {
     public static final String USER = "user";
     public static final String SIZE = "size";
     public static final String TIME = "time";
+    static final int SEGON = 1000;
 }
